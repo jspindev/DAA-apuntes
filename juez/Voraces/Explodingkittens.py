59cbd8595a134a7c502b5881d021cae01531e4af
jspin <javier.espin@hotmail.es>
2022-03-23 11:52:37 +0100

    create Explodingkittens

---
+++ juez/Voraces/Explodingkittens.py
@@ -0,0 +1,10 @@
+
+# no se si es waiting time o mochila sindeo el risgo lo maximo que me cuadra
+
+
+n,m = map(int, input().strip().split())
+task = []
+for _ in range(n):
+    c,r,b = input().strip().split()
+
+print(c,r,b)
