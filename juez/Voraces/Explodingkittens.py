df0f1f3ae28a2ee891f9148d641ae4157a41e561
jspin <javier.espin@hotmail.es>
2022-03-29 13:20:01 +0200

    explodingkittens intento

--- juez/Voraces/Explodingkittens.py
+++ juez/Voraces/Explodingkittens.py
@@ -1,62 +1,32 @@
 
-# no se si es waiting time o mochila sindeo el risgo lo maximo que me cuadra
-
-def isFeasible(data, bestItem, freeWeight):
-    return (freeWeight - data['weight'][bestItem]) >= 0
-
-def getBestItem(data, candidates):
-    bestRatio = 0
-    bestItem = -1
-    for c in candidates:
-        r = data['profit'][c] / data['weight'][c]
-        if r > bestRatio:
-            bestRatio = r
-            bestItem = c
-    return bestItem
-
-
-def greedyknapsack(data):
-
-    n= len(data["profit"])
-    candidates = set()
-    for i in range(n):
-        candidates.add(i)
-    sol = [0] * n
-
-    freeWeight = data["maxWeight"]
-    isSol = False
-    val = 0
-    while candidates and not isSol :
-        bestItem= getBestItem(data, candidates)
-        candidates.remove(bestItem)
-        if isFeasible(data, bestItem, freeWeight):
-            sol[bestItem] = 1.0
-            val += data['profit'][bestItem]
-            freeWeight -= data['weight'][bestItem]
+def explodingkittens(data, m, data_aux):
+    riesgo = 0
+    beneficio = 0
+    ataques = []
+    sol = []
+    while riesgo < m:
+        act_data = data.pop(0)
+        ataques.append(act_data)
+        if riesgo + act_data[3] <= m:
+            riesgo += act_data[3]
+            beneficio += act_data[2]
         else:
-            sol[bestItem] =  freeWeight /data['weight'][bestItem]
-            val += data['profit'][bestItem] * sol[bestItem]
-            isSol = True
-    return sol, val
+            riesgo_proporcional = m - riesgo
+            beneficio += (riesgo_proporcional/act_data[3]) * act_data[2]
+            riesgo *= act_data[3]
+    for a in ataques:
+        sol.append(ataques[a][1])
+    print(*sol)
 
-n,m = map(int, input().strip().split()) #m riesgo
-data = {}
-data["attack"]=[]
-data["profit"]=[]
-data["weight"]=[]
-ataque=[]
-profit=[]
-weight=[]
-for i in range(n):
+
+n,m = map(int, input().strip().split())
+data = []
+
+for _ in range(n):
     c, r, b = input().strip().split()
-    ataque.append(c)
-    profit.append(b)
-    weight.append(r)
-data["attack"]=ataque
-data["profit"]=profit
-data["weight"]=weight
-data["maxWeight"] = m
+    r = int(r)
+    b = int(b)
+    data.append((r/b, c, b, r))
 
-(sol, val) = greedyknapsack(data)
-
-print(*sol)
+data_aux = sorted(data)
+explodingkittens(data_aux, m, data)
