eef52f1510cd3c0eec00d22d9f5859c7516cbd96
jspin <javier.espin@hotmail.es>
2022-04-28 16:38:37 +0200

    mochilabacktraking

---
+++ t6 backtracking/mochilaBackTracking.py
@@ -0,0 +1,32 @@
+def inicializarDatos():
+    datos = {}
+    datos['n'] = 4
+    datos['w'] = 8
+    datos['peso'] = [2,3,4,5]
+    datos['valor'] = [3,5,6, 10]
+
+
+def inicializarSolucion(datos):
+    sol = {}
+    sol['Objetos'] = [0] * datos['N']
+    sol['Peso'] = 0
+    sol['valor'] = 0
+
+    return sol
+
+def esSolucion(sol,datos):
+    return sol['Peso'] + min(datos['Peso']) > datos['w']
+#otra opcion peor
+#def esSolucion(k,sol):
+    #return k == len(sol['Objetos'] -1)
+    #return k == datos['N']
+def mochilaVA(sol, mejorSol, datos, k):
+    if esSolucion():
+
+
+#prog.principal
+datos = inicializarDatos()
+sol = inicializarSolucion(datos)
+mejorsol = inicializarSolucion(datos)
+k=0
+mejorsol = mochilaVA(sol,mejorsol, datos, k)
