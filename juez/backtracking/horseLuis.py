c2dbb0a78e8b458e4d964e741e856e5eff063859
jspin <javier.espin@hotmail.es>
2022-05-10 17:58:53 +0200

    ejercicio s

--- juez/backtracking/horseLuis.py
+++ juez/backtracking/horseLuis.py
@@ -1,44 +1,40 @@
-def esFactible(cell,dirX,dirY,mat,lastObject,visited):
-    nextX = cell[0] + dirX
-    nextY = cell[1] + dirY
-    return (0 <= nextX < len(mat)) and (0 <= nextY <len(mat[0])) and \
-           (mat[nextX][nextY] == lastObject+1 or mat[nextX][nextY] == 0) and \
-           mat[nextX][nextY] not in visited
+dirsX = [1, 0, -1, 0]
+dirsY = [0, 1, 0, -1]
 
-def bt(mat, cell, p, lastObject, actMin, actStep, visited):
-    if lastObject == p:
-        actMin = min(actMin, actStep)
+
+def bt(mat, cell, p, lastObject, actMin, actSteps, visited):
+    if lastObject == p :
+        actMin = min(actMin, actSteps)
     else:
-        dirX = [1,0,-1,0]
-        dirY = [0,1,0,-1]
-        for a in range(len(dirX)):
-            updateObject = False
-            if esFactible(cell,dirX[a], dirY[a], mat, lastObject, visited):
-                nextX = cell[0] + dirX
-                nextY = cell[1] + dirY
+        for a in range(len(dirsX)):
+            updatedObject = False
+            if isFeasible(cell, dirsX[a], dirsY[a], mat, lastObject, visited):
+                nextX = cell[0] + dirsX[a]
+                nextY = cell[1] + dirsY[a]
                 visited.add((nextX, nextY))
-                if mat[nextX][nextY] == lastObject-1:
+                if mat[nextX][nextY] == lastObject + 1:
                     lastObject += 1
-                    updateObject = True
-                act = min(actMin, bt(mat,(nextX, nextY), p, lastObject, actMin, actStep+1, visited))
-                visited.remove((nextX,nextY))
-                if updateObject:
-                    lastObject -=1
+                    updatedObject = True
+                actMin = min(bt(mat, (nextX, nextY), p, lastObject, actMin, actSteps + 1, visited), actMin)
+                if updatedObject:
+                    lastObject -= 1
+                visited.remove((nextX, nextY))
     return actMin
 
 
+def isFeasible(cell, dirX, dirY, mat, lastObject, visited):
+    nextX = cell[0] + dirX
+    nextY = cell[1] + dirY
+    return 0 <= nextX < len(mat) and 0 <= nextY < len(mat[0]) and (mat[nextX][nextY] == lastObject + 1 or mat[nextX][nextY] == 0) and (nextX, nextY) not in visited
 
 
-
-
-n, m, p =  map(int, input().strip().split())
-mat = []
-for _ in range(n):
-    row = list(map(int, input().strip().split()))
-    mat.append(row)
-
-init_cell =(0,0)
-visited  = set()
-visited.add(init_cell)
-result = bt(mat, init_cell, p, init_cell, 0, 0, visited)
-print(result)
+if __name__ == '__main__':
+    n, m, p = map(int, input().strip().split())
+    mat = []
+    for _ in range(n):
+        row = list(map(int, input().strip().split()))
+        mat.append(row)
+    init_cell = (0, 0)
+    visited = set()
+    visited.add(init_cell)
+    print(bt(mat, init_cell, p, 0, 0x3f3f3f, 1, visited))
